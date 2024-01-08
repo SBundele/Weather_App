4b6d4814d404df603fb48b52dc57c0bfdefa45b9
Sourabh Bundele <sbundele0318@gmail.com>
2024-01-08 14:35:53 +0530

    using requests library fetched temperature

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import requests
+
 api_key = "6311bdd8055d12e9d877464f3c124e12"
 
 # Took the url from open whether api
@@ -7,4 +9,10 @@ url = "https://api.openweathermap.org/data/2.5/weather?units=metric&"
 city_name = input("Enter city name: ")
 
 # Created final url with addition of api_key and city_name
-Final_url = url + "appid" + api_key + "&q" + city_name
+Final_url = url + "appid=" + api_key + "&q=" + city_name
+
+response = requests.get(Final_url).json()
+
+temperature = response['main']['temp']
+
+print(temperature)
