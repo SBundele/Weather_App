2a26a68565d462971c0f4db021d7688d5843ab48
Sourabh Bundele <sbundele0318@gmail.com>
2024-01-08 14:17:12 +0530

    created final url using city_name

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 api_key = "6311bdd8055d12e9d877464f3c124e12"
+
+# Took the url from open whether api
+url = "https://api.openweathermap.org/data/2.5/weather?units=metric&"
+
+# Took the input from the user
+city_name = input("Enter city name: ")
+
+# Created final url with addition of api_key and city_name
+Final_url = url + "appid" + api_key + "&q" + city_name
